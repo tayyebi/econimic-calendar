6cd01792e29b78d068dbaa729ce1174c0125859e
tayyebi <smile@tyyi.net>
2024-01-22 12:11:56 +0330

    Added ecocal.

---
+++ main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    var events = [];
+    var files = [
+        './fx.ics',
+        './iran-holidays.ics'
+    ];
+    $.each(files, function (index, file) {
+        $.ajax({
+            url: file,
+            dataType: 'text',
+            success: function (data) {
+                var jcalData = ICAL.parse(data);
+                var comp = new ICAL.Component(jcalData);
+                var eventComps = comp.getAllSubcomponents('vevent');
+                $.each(eventComps, function (index, item) {
+                    var event = {
+                        title: item.getFirstPropertyValue('summary'),
+                        start: item.getFirstPropertyValue('dtstart').toJSDate(),
+                        end: item.getFirstPropertyValue('dtend').toJSDate(),
+                        location: item.getFirstPropertyValue('location')
+                    };
+                    events.push(event);
+                });
+                $('#calendar').fullCalendar({
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay'
+                    },
+                    defaultDate: '2024-01-22',
+                    navLinks: true,
+                    editable: true,
+                    eventLimit: true,
+                    events: events
+                });
+            }
+        });
+    });
+});
